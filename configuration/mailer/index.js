f6193767386b82ef5a6968ed5619a5ce6294a1ea
KrystianMigura <Krystian.migura@gmail.com>
2019-09-13 20:03:07 +0200

    correct send email

--- configuration/mailer/index.js
+++ configuration/mailer/index.js
@@ -36,14 +36,14 @@ var account = new Promise(function(resolve, reject){
 }
 //dodać całą resztę!
 module.exports.sendEmail = function(transporter){
-
+    var html = "Dziękujemy za rejestracje na naszej stronie<br> Poniżej znajduje się link w celach potwierdzenia rejestracjii.<br> Gdy zostaną państwo przekierowani na stronę główną, to autoryzacja przebiegła pomyślnie<br> W razie pytań proszę o kontakt. <br>"
     let info = new Promise(function(resolve, reject){
 
         const mailOptions = {
             from: 'simplegamesofdev@gmail.co', // sender address
             to: transporter.userInformation.email, // list of receivers
-            subject: 'Potwierdzenie rejestracji użytkownika', // Subject line
-            html: "http://192.168.1.106/register&data="+transporter.userInformation.name+"&"+transporter.userInformation.nick+"&"+transporter.userInformation.uniqueId+"" // plain text body
+            subject: 'Potwierdzenie rejestracji użytkownika na Stronie SimpleGameSofDev', // Subject line
+            html: html + "Link: http://192.168.1.106/register&data="+transporter.userInformation.name+"&"+transporter.userInformation.nick+"&"+transporter.userInformation.uniqueId+""
         };
 
          transporter.transporter.sendMail(mailOptions, function (err, info) {
